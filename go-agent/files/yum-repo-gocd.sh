8a2bfd89536cb788ab08d1c99eb881ab099b57ee
ylzheng <yunl.zheng@gmail.com>
2016-03-25 17:23:00 +0800

    [#0] create ansible script provision deli enviroment

---
+++ go-agent/files/yum-repo-gocd.sh
@@ -0,0 +1,8 @@
+echo "
+[gocd]
+name     = GoCD YUM Repository
+baseurl  = https://download.go.cd
+enabled  = 1
+gpgcheck = 1
+gpgkey   = https://download.go.cd/GOCD-GPG-KEY.asc
+" | sudo tee /etc/yum.repos.d/gocd.repo
